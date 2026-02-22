fdd80a9f365c7392865ff2f3fec38e26907e88a4
Alind Dwivedi <alinddwivedi@ADWIVEDI-PAT-IN.local>
2026-02-22 12:28:12 +0530

    fix: add github pages basepath

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/alind-portfolio",
+  assetPrefix: "/alind-portfolio/",
   images: {
     unoptimized: true,
   },
